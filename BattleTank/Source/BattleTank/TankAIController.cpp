9bae0e998f50eefeaa6d1a7efb209b5fe936853e
HP-470G2-LCT\Kirill.Semenov <cyril.semenov@gmail.com>
2018-03-25 13:41:44 +0300

    BT013 Added AI controller

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -2,6 +2,20 @@
 
 #include "TankAIController.h"
 
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) { return; }
+	UE_LOG(LogTemp, Warning, TEXT("%s AI Begin Play"), *(ControlledTank->GetName()));
+	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play"));
+}
+
+ATank * ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
 
 
 
--- BattleTank/Source/BattleTank/TankAIController.h
+++ BattleTank/Source/BattleTank/TankAIController.h
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "Tank.h"
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
@@ -14,7 +14,9 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+public:
+	ATank * GetControlledTank() const;
+
+	void BeginPlay() override;
+		
 };
