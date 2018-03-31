6865e76d3bc1ebbd9f9822e87069516361ed2f2f
HP-470G2-LCT\Kirill.Semenov <cyril.semenov@gmail.com>
2018-03-31 21:41:46 +0300

    BT031 Added reload time

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -7,38 +7,23 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	auto ControlledTank = GetControlledTank();
-	auto PlayerTank = GetPlayerTank();
-	if (!ControlledTank) { return; }
-	if (!PlayerTank) { return; }
-	UE_LOG(LogTemp, Warning, TEXT("%s AI Begin Play"), *(ControlledTank->GetName()));
-	UE_LOG(LogTemp, Warning, TEXT("AI has found a human player: %s"), *(PlayerTank->GetName()));
+
 }
 
 void ATankAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (GetPlayerTank()) 
+	
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	auto ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank) 
 	{
 		// TODO Move towards the player
-		
+				
 		// Aim towards the player
-		GetControlledTank()->AimAt(GetPlayerTank()->GetTargetLocation());
-
-		// TODO Fire if ready
+		ControlledTank->AimAt(PlayerTank->GetTargetLocation());
+		ControlledTank->Fire(); // TODO Make an interval between shots
 	}
 }
-
-// Get AI controlled pawn
-ATank * ATankAIController::GetControlledTank() const
-{
-	return Cast<ATank>(GetPawn());
-}
-
-// Get human controlled pawn
-ATank * ATankAIController::GetPlayerTank() const
-{
-	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
-}
-
 
