ea66086cf9a430df254c20cf1b31faf3e08e626e
HP-470G2-LCT\Kirill.Semenov <cyril.semenov@gmail.com>
2018-04-08 20:32:45 +0300

    BT056 Projectiles now destroyed on impact (also projectile-type objects are destroyed on timer)

--- BattleTank/Source/BattleTank/Projectile.h
+++ BattleTank/Source/BattleTank/Projectile.h
@@ -3,7 +3,7 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "GameFramework/ProjectileMovementComponent.h"
-#include "GameFramework/Actor.h"
+#include "PhysicsEngine/RadialForceComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 #include "Projectile.generated.h"
 UCLASS()
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	void LaunchProjectile(float Speed);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -24,12 +26,16 @@ protected:
 	UStaticMeshComponent * CollisionMesh = nullptr;
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent * LaunchBlast = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent * ImpactBlast = nullptr;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	URadialForceComponent * ExplosionForce = nullptr;
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float DestroyDelay = 10;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
-	void LaunchProjectile(float Speed);
-	
-	
+	void OnTimerExpire();
 };
